9e9f31ea5d97ec30f2bb3a3445aa4e17d7ecc770
heevic <dbgmltmd0317@gmail.com>
2023-05-05 15:55:23 +0900

    fix: Week랑 Time이 바뀐 오류 수정

--- HeeSeung-Yoo/TimeTableFE/main.js
+++ HeeSeung-Yoo/TimeTableFE/main.js
@@ -10,8 +10,28 @@ Close.addEventListener("click", e => {
     pop.style.display = "none"
 });
 
-Add.addEventListener('click', (e) => add(e));
+const Hour = document.querySelector('.hour').textContent;
+const Mon = document.querySelector('.mon');
+const Tue = document.querySelector('.tue');
+const Wed = document.querySelector('.wed');
+const Thu = document.querySelector('.thu');
+const Fri = document.querySelector('.fri');
 
-function add(e) {
+const selectTime = document.querySelector('#selecttime')
+const selectWeek = document.querySelector('#selectweek')
+
+
+const Subject = document.querySelector('#subject');
+const Professor = document.querySelector('#professor');
+const SubjectValue = Subject.value;
+const ProfessorValue = Professor.value;
+
+Add.addEventListener("click", add);
+function add() {
+    const selectWeekValue = (selectWeek.options[selectWeek.selectedIndex].value);
+    const selectTimeValue = (selectTime.options[selectTime.selectedIndex].value);
     
+  
 }
+// if(document.querySelector('#aside-manage div') != null)
+// alert('요소 존재');
